2fe9e8f5d3a71080a815723956150bf4755a7f09
Abdullahi254 <abdush504@gmail.com>
2023-01-13 23:31:16 +0300

    integrated the reducer of reducing item quantity to the Cart route component

--- components/CartCard.tsx
+++ components/CartCard.tsx
@@ -3,27 +3,30 @@ import React from 'react'
 import testImg from "../public/test3.png"
 import { AiOutlineUp, AiOutlineDown } from 'react-icons/ai'
 import type { RootState } from '../redux/app/store'
-import {useDispatch} from "react-redux"
-import {addToCart, removeFromCart} from '../redux/features/cart/cartSlice'
+import { useDispatch } from "react-redux"
+import { addToCart, removeFromCart, reduceItemQuantity } from '../redux/features/cart/cartSlice'
 type Props = {
-    item:ReturnType<(state:RootState)=>typeof state.cart.items[0]>
-    index:number
+    item: ReturnType<(state: RootState) => typeof state.cart.items[0]>
+    index: number
 }
 
-const CartCard = ({item,index}: Props) => {
+const CartCard = ({ item, index }: Props) => {
     const dispatch = useDispatch()
-    const removeHandler = ()=>{
+    const removeHandler = () => {
         dispatch(removeFromCart(index))
     }
-    const increaseItemQuantity = ()=>{
+    const increaseItemQuantity = () => {
         const newItem = {
             ...item,
             variant: item.variant,
             price: item.price,
             quantity: 1,
             subTotal: item.price * 1,
-          }
+        }
         dispatch(addToCart(newItem))
+    }
+    const reduceItem = () => {
+        dispatch(reduceItemQuantity(index))
     }
     return (
         <div className='bg-gray-100 p-6 flex justify-between items-center rounded-md relative'>
@@ -43,14 +46,14 @@ const CartCard = ({item,index}: Props) => {
             </div>
 
             <div className='absolute left-1/2 hidden md:flex flex-col items-center space-y-1'>
-                <AiOutlineUp className='text-gray-400 hover:text-indigo-600 cursor-pointer' onClick={increaseItemQuantity}/>
+                <AiOutlineUp className='text-gray-400 hover:text-indigo-600 cursor-pointer' onClick={increaseItemQuantity} />
                 <span>{item.quantity}</span>
-                <AiOutlineDown className='text-gray-400 hover:text-indigo-600 cursor-pointer' />
+                <AiOutlineDown className='text-gray-400 hover:text-indigo-600 cursor-pointer' onClick={reduceItem} />
             </div>
 
             <div className=' flex flex-col items-center'>
                 <p className=' font-semibold'>KSH{item.subTotal.toFixed(2)}</p>
-                <p className=' text-xs text-gray-400'>KSH{(item.subTotal/item.quantity).toFixed(2)} each</p>
+                <p className=' text-xs text-gray-400'>KSH{(item.subTotal / item.quantity).toFixed(2)} each</p>
             </div>
 
         </div>
