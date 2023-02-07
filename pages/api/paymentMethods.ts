1fcdb90390d762e6485ab49b96f177383c111153
Abdullahi254 <abdush504@gmail.com>
2023-02-07 23:12:18 +0300

    testing bug

--- pages/api/paymentMethods.ts
+++ pages/api/paymentMethods.ts
@@ -1,39 +1,50 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import NextCors from 'nextjs-cors'
+// import NextCors from 'nextjs-cors'
+
+// type Data = {
+//   id?:string
+//   name?:string
+//   checkoutUrl?:string
+//   error?:string
+// }
+
+// export default async function handler(
+//   req: NextApiRequest,
+//   res: NextApiResponse<Data[]>
+// ) {
+//   // Run the middleware
+//   await NextCors(req, res, {
+//     // Options
+//     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+//     origin: '*',
+//     optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+//   })
+
+//   if (req.method === 'POST') {
+//     const publicToken = req.body.publicToken
+//     const response = await fetch(`https://payment.snipcart.com/api/public/custom-payment-gateway/validate?publicToken=${publicToken}`)
+//     if (response.ok) {
+//       let paymentMethodList = [{
+//         id: 'M-pesa_gateway',
+//         name: 'M-Pesa gateway',
+//         checkoutUrl: 'https://dukamoto.vercel.app/paymentCheckout',
+//       }]
+//       res.status(200).json(paymentMethodList)
+//     } else {
+//       res.status(403).json([{error:'Request not allowed'}]);
+//     }
+//   } else {
+//     res.status(400).json([{error:'Request sent Incorrectly'}]);
+//   }
+// }
 
 type Data = {
-  id?:string
-  name?:string
-  checkoutUrl?:string
-  error?:string
+  name: string
 }
 
-export default async function handler(
+export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data[]>
+  res: NextApiResponse<Data>
 ) {
-  // Run the middleware
-  await NextCors(req, res, {
-    // Options
-    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-    origin: '*',
-    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-  })
-
-  if (req.method === 'POST') {
-    const publicToken = req.body.publicToken
-    const response = await fetch(`https://payment.snipcart.com/api/public/custom-payment-gateway/validate?publicToken=${publicToken}`)
-    if (response.ok) {
-      let paymentMethodList = [{
-        id: 'M-pesa_gateway',
-        name: 'M-Pesa gateway',
-        checkoutUrl: 'https://dukamoto.vercel.app/paymentCheckout',
-      }]
-      res.status(200).json(paymentMethodList)
-    } else {
-      res.status(403).json([{error:'Request not allowed'}]);
-    }
-  } else {
-    res.status(400).json([{error:'Request sent Incorrectly'}]);
-  }
+  res.status(200).json({ name: 'John Doe' })
 }
